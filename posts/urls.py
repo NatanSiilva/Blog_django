37ade038d8bba893314fb8c41897cccdfe0c6a67
Natanael <contato@natandev.com.br>
2021-11-12 11:48:43 -0300

    feat: views

--- posts/urls.py
+++ posts/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.PostIndex.as_view(), name='index'),
-    path('categories/<str:categories>',
-         views.PostCategories.as_view(), name='post_categories'),
+    path('categories/<str:category>',
+         views.PostCategories.as_view(), name='post_category'),
     path('search/', views.PostSearch.as_view(), name='post_search'),
     path('post/<int:pk>', views.PostDetails.as_view(), name='post_details'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -2,21 +2,68 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic import UpdateView
 from .models import Post
+from django.db.models import Q, Count, Case, When
 
 
 class PostIndex(ListView):
     model = Post
     template_name = 'posts/index.html'
-    paginate_by = 5
+    paginate_by = 10
     context_object_name = 'posts'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        # qs = qs.order_by('-id').filter(published_post=True)
+        qs = qs.order_by('-id')
+        qs = qs.annotate(
+            num_comments=Count(
+                Case(
+                    When(
+                        comment__publish_comment=True, then=1
+                    )
+                )
+            )
+        )
+
+        return qs
 
 
 class PostSearch(PostIndex):
-    pass
+    template_name = 'posts/post_search.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        serach = self.request.GET.get('search')
+
+        if not serach:
+            return qs
+
+        qs = qs.filter(
+            Q(title_post__icontains=serach) |
+            Q(author_post__first_name__iexact=serach) |
+            Q(content_post__icontains=serach) |
+            Q(excerpt_post__icontains=serach) |
+            Q(category_post__name_cat__iexact=serach)
+        )
+
+        return qs
 
 
 class PostCategories(PostIndex):
-    pass
+    template_name = 'posts/post_category.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        category = self.kwargs.get('category', None)
+
+        if not category:
+            return qs
+
+        qs = qs.filter(category_post__name_cat__iexact=category)
+
+        return qs
 
 
 class PostDetails(UpdateView):
