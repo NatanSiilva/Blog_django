b04d507710570cb05c19b1aa3dd773406eba112d
Natanael <natanaelsilva.progjs@gmail.com>
2021-11-22 23:46:56 -0300

    refactor: model post, resize image

--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from categories.models import Category
 from django.contrib.auth.models import User
 from django.utils import timezone
+from PIL import Image
+from django.conf import settings
+import os
 
 
 class Post(models.Model):
@@ -18,3 +21,23 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title_post
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        self.resize_image(self.image_post.name, 800)
+
+    @staticmethod
+    def resize_image(name_image, new_width):
+        image_path = os.path.join(settings.MEDIA_ROOT, name_image)
+        image = Image.open(image_path)
+        width, height = image.size
+        new_height = round((height * new_width) / width)
+
+        if width <= new_width:
+            image.close()
+            return
+
+        new_image = image.resize((new_width, new_height), Image.ANTIALIAS)
+        new_image.save(image_path, optimize=True, quality=60)
+        new_image.close()
